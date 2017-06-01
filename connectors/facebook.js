827f54c77e3339ca9072ac59b9faafe0f3ab95c3
Harry Mumford-Turner <harrymt@me.com>
2017-06-01 19:55:58 +0100

    Adhered whole codebase to jshint

--- connectors/facebook.js
+++ connectors/facebook.js
@@ -1,7 +1,7 @@
-'use strict'
+'use strict';
 
-var request = require('request')
-var Config = require('../config')
+var request = require('request');
+var Config = require('../config');
 
 // SETUP A REQUEST TO FACEBOOK SERVER
 var newRequest = request.defaults({
@@ -14,7 +14,7 @@ var newRequest = request.defaults({
 	headers: {
 		'Content-Type': 'application/json'
 	},
-})
+});
 
 // SETUP A MESSAGE FOR THE FACEBOOK REQUEST
 var newMessage = function (recipientId, msg, atts, cb) {
@@ -24,7 +24,7 @@ var newMessage = function (recipientId, msg, atts, cb) {
 				id: recipientId
 			},
 		}
-	}
+	};
 
 	// https://developers.facebook.com/docs/messenger-platform/send-api-reference
 
@@ -61,28 +61,29 @@ var newMessage = function (recipientId, msg, atts, cb) {
 	//   }
 	// }
 
+	var message = null;
 	if (atts) {
-		var message = {
+		message = {
 			attachment: {
 				"type": "image",
 				"payload": {
 					"url": msg
 				}
 			}
-		}
+		};
 	} else {
-		var message = {
+		message = {
 			text: msg
-		}
+		};
 	}
-	opts.form.message = message
+	opts.form.message = message;
 
 	newRequest(opts, function (err, resp, data) {
 		if (cb) {
-			cb(err || data.error && data.error.message, data)
+			cb(err || data.error && data.error.message, data);
 		}
-	})
-}
+	});
+};
 
 // PARSE A FACEBOOK MESSAGE to get user, message body, or attachment
 // https://developers.facebook.com/docs/messenger-platform/webhook-reference
@@ -95,12 +96,12 @@ var getMessageEntry = function (body) {
 						body.entry[0].messaging &&
 						Array.isArray(body.entry[0].messaging) &&
 						body.entry[0].messaging.length > 0 &&
-						body.entry[0].messaging[0]
-	return val || null
-}
+						body.entry[0].messaging[0];
+	return val || null;
+};
 
 module.exports = {
 	newRequest: newRequest,
 	newMessage: newMessage,
 	getMessageEntry: getMessageEntry,
-}
+};
