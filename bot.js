0d55a0e49685944b5938405ecab03b246302d184
Harry Mumford-Turner <harrymt@me.com>
2017-06-11 18:25:18 +0100

    Reformatted bot responses
    - If the bot has see you before it realises
    - Bot updates your information in settings (type 'settings/stats/Settings' to see)
    - Bot saves your information only when you update it
    - Correctly set against fbid rather than a random id
    - Remove unused code
    - Wrote updateuser function

--- bot.js
+++ bot.js
@@ -21,6 +21,54 @@ function getDatastore() {
   );
 }
 
+/**
+ * Update datastore with new user information
+ *
+ * @param user
+ * @param callback
+ */
+const updateUser = (user, callback) => {
+  const datastore = getDatastore();
+
+  // Query our datastore for this fbid user
+  const query = datastore.createQuery('User').filter('fbid', '=', user.fbid);
+  datastore.runQuery(query, (err, entities, info) => {
+    if (err) {
+      console.log(err);
+      console.log('Error running query, with User=fbid');
+      throw new Error('Error running query with User=' + user.fbid);
+    }
+
+    console.log(entities.length + ' users found.');
+
+    // Check if  more results may exist.
+    if (entities.length === 0 && info.moreResults === datastore.NO_MORE_RESULTS) {
+      console.log('No user found, error!');
+      throw new Error('Error updating User=' + user.fbid + ', no user found!');
+    } else {
+      console.log('User found in datastore.');
+      console.log(entities[0]);
+
+      console.log('Updating with new user info...');
+      console.log(user);
+
+      const key = datastore.key(['User', user.fbid]);
+      datastore.save({
+        key,
+        data: user
+      }, err => {
+        if (!err) {
+          // The user is now published!
+          callback();
+        } else {
+          console.log(err);
+          throw new Error('Error updating User=' + user.fbid + ': err: ' + err);
+        }
+      });
+    }
+  });
+};
+
 const findOrCreateUser = (fbid, callback) => {
   const datastore = getDatastore();
 
@@ -42,10 +90,12 @@ const findOrCreateUser = (fbid, callback) => {
       // Not user found! Lets create one! Then return the fbid
       const userData = {
         fbid,
+        modality: null,
+        seen_before: false,
         reminder_time: null
       };
 
-      const key = datastore.key('User');
+      const key = datastore.key(['User', fbid]);
 
       datastore.save({
         key,
@@ -58,147 +108,145 @@ const findOrCreateUser = (fbid, callback) => {
       });
     } else {
       console.log('User found.');
-      console.log(entities);
+      console.log(entities[0]);
 
       // Return the actual user object
-      callback(entities);
+      callback(entities[0]);
     }
   });
-
-  // Const user = {
-  //   key: datastore.key(['Users', 'User', 'Harry']),
-  //   data: {
-  //     fbid: 123,
-  //     msg_time: 'morning'
-  //   }
-  // };
-
-  // datastore.save(user).then(() => {
-  //   console.log(`Saved ${user.key.name}: ${user.data.msg_time}`);
-  // })
-  // .catch((err) => {
-  //   console.error('ERROR:', err);
-  // });
-
-  // // DOES USER SESSION ALREADY EXIST?
-  // Object.keys(sessions).forEach(i => {
-  //   if (sessions[i].fbid === fbid) {
-  //     console.log('User session already exists with fb');
-  //     // YUP
-  //     sessionId = i;
-  //   }
-  // });
-
-  // // No session so we will create one
-  // if (!sessionId) {
-  //   sessionId = new Date().toISOString();
-  //   console.log('User session does not exist, creating one...');
-
-  //   sessions[sessionId] = {
-  //     fbid: fbid,
-  //     context: {
-  //       _fbid_: fbid
-  //     }
-  //   };
-  // }
-
-  // return sessionId;
 };
+
+/**
+ * Creates a quick reply.
+ *
+ * @param message String to send.
+ * @param options ['reply1', 'reply2'] Array of buttons.
+ * @returns {{text: *, quick_replies: Array}}
+ */
+function createQuickReply(message, options) {
+  let replies = [];
+
+  options.forEach(function(el) {
+    replies.push(
+      {
+        content_type: 'text',
+        title: el,
+        payload: 'PICKED_' + el.replace(' ', '_').toUpperCase()
+      }
+    );
+  });
+
+  return {
+    text: message,
+    quick_replies: replies
+  };
+}
+
+/**
+ * Convert to Pascal Case.
+ *
+ * @param str Unfriendly string.
+ * @returns {string}
+ */
+function convertToFriendlyName(str) {
+  console.log('Converting... : ' + str);
+  return str.replace('_', ' ').split(' ').map(w => w[0].toUpperCase() + w.substr(1).toLowerCase()).join(' ');
+}
 
 const read = function (sender, message, reply) {
   // Let's find the user object
   findOrCreateUser(sender, user => {
-    if (message.quick_reply === undefined) {
-      // If they are new, then send them the standard reply:
-      const replies = [
-        {
-          content_type: 'text',
-          title: 'Stretch',
-          payload: 'DEVELOPER_DEFINED_PAYLOAD_FOR_PICKING_STRETCH'
-        },
-        {
-          content_type: 'text',
-          title: 'Meditate',
-          payload: 'DEVELOPER_DEFINED_PAYLOAD_FOR_PICKING_MEDITATE'
-        },
-        {
-          content_type: 'text',
-          title: 'Drink Water',
-          payload: 'DEVELOPER_DEFINED_PAYLOAD_FOR_PICKING_DRINK_WATER'
-        }
-      ];
 
-      message = {
-        text: 'Yo, I am Arthur. What habit are you tracking?',
-        quick_replies: replies
-      };
+    let messageStart = '';
 
-      reply(sender, message);
+    // If we have seen this user before, send them a greeting
+    if (user.seen_before) {
+      messageStart = 'Welcome back! ';
     } else {
-      // STRETCHING
-      if (message.quick_reply.payload === 'DEVELOPER_DEFINED_PAYLOAD_FOR_PICKING_STRETCH') {
-          const replies = [
-            {
-              content_type: 'text',
-              title: 'Morning',
-              payload: 'DEVELOPER_DEFINED_PAYLOAD_FOR_PICKING_MORNING'
-            },
-            {
-              content_type: 'text',
-              title: 'Afternoon',
-              payload: 'DEVELOPER_DEFINED_PAYLOAD_FOR_PICKING_AFTERNOON'
-            },
-            {
-              content_type: 'text',
-              title: 'Evening',
-              payload: 'DEVELOPER_DEFINED_PAYLOAD_FOR_PICKING_DRINK_EVENING'
-            }
-          ];
+      user.seen_before = true;
+      messageStart = 'Hello new person! ';
+    }
 
-          const msg = message;
-          message = {
-            text: 'Cool I love to ' + msg.text + '. What time do you want to ' + msg.text + '?',
-            quick_replies: replies
-          };
+    if (message.quick_reply === undefined) {
+      if (message && (message.toLowerCase() === 'stats' || message.toLowerCase() === 'settings')) {
+        console.log(user.toString());
+        reply(sender,
+          { message: 'Your settings are: ' + JSON.stringify(user) }
+        );
+      } else {
+        reply(sender,
+          createQuickReply(
+            messageStart + 'I\'m Arthur. I\'m not a talkative bot. What habit do you want to track?',
+            [
+              'Stretch',
+              'Meditate',
+              'Drink Water'
+            ]
+          )
+        );
+      }
+    } else {
+      if (message.quick_reply.payload === 'PICKED_STRETCH' ||
+          message.quick_reply.payload === 'PICKED_MEDITATE' ||
+          message.quick_reply.payload === 'PICKED_DRINK_WATER') {
 
-          reply(sender, message);
+        const habit = message.quick_reply.payload.substring(7);
 
-      } else if (message.quick_reply.payload === 'DEVELOPER_DEFINED_PAYLOAD_FOR_PICKING_MORNING') {
-          const replies = [
-            {
-              content_type: 'text',
-              title: 'Visual',
-              payload: 'DEVELOPER_DEFINED_PAYLOAD_FOR_PICKING_VISUAL'
-            },
-            {
-              content_type: 'text',
-              title: 'Sound',
-              payload: 'DEVELOPER_DEFINED_PAYLOAD_FOR_PICKING_SOUND'
-            },
-            {
-              content_type: 'text',
-              title: 'Vibration',
-              payload: 'DEVELOPER_DEFINED_PAYLOAD_FOR_PICKING_VIBRATION'
-            }
-          ];
+        // Save habit against user
+        user.habit = habit;
 
-          message = {
-            text: 'Nice, I will remind you around that time, what mode of reward would you like?',
-            quick_replies: replies
-          };
+        // Save user information to datastore
+        updateUser(user, () => {
+          // Then reply
+          reply(sender,
+              createQuickReply(
+                  'That\'s a good one, what time would you like a reminder to ' + convertToFriendlyName(habit) + '?',
+                  [
+                      'Morning',
+                      'Afternoon',
+                      'Evening'
+                  ]
+              )
+          );
+        });
 
-          reply(sender, message);
+      } else if (message.quick_reply.payload === 'PICKED_MORNING' ||
+                 message.quick_reply.payload === 'PICKED_AFTERNOON' ||
+                 message.quick_reply.payload === 'PICKED_EVENING') {
 
-      } else if (message.quick_reply.payload === 'DEVELOPER_DEFINED_PAYLOAD_FOR_PICKING_VISUAL') {
-        message = {
-          text: 'Sweet. I will catch you later.'
-        };
-        reply(sender, message);
+        const timeOfDay = message.quick_reply.payload.substring(7);
+        user.reminder_time = timeOfDay;
+        // Save user information to datastore
+        updateUser(user, () => {
+          reply(sender,
+              createQuickReply(
+                  'Nice, I will remind you in the ' + convertToFriendlyName(timeOfDay) + ', what mode of reward would you like?',
+                  [
+                      'Visual',
+                      'Sound',
+                      'Vibration'
+                  ]
+              )
+          );
+        });
+      } else if (message.quick_reply.payload === 'PICKED_VISUAL' ||
+                 message.quick_reply.payload === 'PICKED_SOUND' ||
+                 message.quick_reply.payload === 'PICKED_VIBRATION') {
+
+        const modality = message.quick_reply.payload.substring(7);
+        user.modality = modality;
+        // Save user information to datastore
+        updateUser(user, () => {
+          reply(sender, {
+            message: modality + ' rewards are the best! See you later!'
+          });
+        });
       }
     }
   });
 };
 
 module.exports = {
-  read
+  read,
+  updateUser
 };
