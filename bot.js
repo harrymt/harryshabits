70d62994e9a43a80e0952bc5d75a5f4ec27b1afa
Harry Mumford-Turner <harrymt@me.com>
2017-06-22 16:47:58 +0100

    Implemented audio and visual rewards

--- bot.js
+++ bot.js
@@ -202,16 +202,16 @@ const read = function (sender, message, reply) {
           database.updateUser(user, () => {
             // Send the modality reward!
             if (user.modality === 'VISUAL') {
-              reply(sender, {
-                text: 'Awesome! Here is your Visual reward.'
-              });
+              reply(sender, getVisualReward());
             } else if (user.modality === 'SOUND') {
               reply(sender, {
-                text: 'Awesome! Here is your Audio reward.'
+                text: getAudioReward()
               });
             } else if (user.modality === 'VIBRATION') {
+              // TODO send fitbit vibration
+
               reply(sender, {
-                text: 'Awesome! Here is your Vibration reward.'
+                text: 'Buzz Buzz Buzz.'
               });
             }
           });
@@ -220,6 +220,47 @@ const read = function (sender, message, reply) {
     }
   });
 };
+
+/**
+ * Choose a random audio reward and wrap it up into an object ready to send.
+ */
+function getAudioReward() {
+  const audioRewards = [
+    'https://open.spotify.com/track/2olVm1lHicpveMAo4AUDRB', // power of love
+    'https://open.spotify.com/track/3fthfkkvy9av3q3uAGVf7U', // Shake it off
+    'https://open.spotify.com/track/6Nf1bklus7o9fpKto13nDc', // OK GO, this shall not pass
+    'https://open.spotify.com/track/6Lphpr9Z6H282Sguw0dUWa' // Ahh Freak out
+  ];
+
+  // Choose a random reward
+  const chosenReward = Math.floor((Math.random() * (audioRewards.length - 1)) + 1);
+
+  return audioRewards[chosenReward];
+}
+
+/**
+ * Wrap a random gif up in an object.
+ */
+function getVisualReward() {
+  const visualRewards = [
+    'https://media.giphy.com/media/XreQmk7ETCak0/giphy.gif', // Boy at computer, thumbs up
+    'https://media.giphy.com/media/XreQmk7ETCak0/giphy.gif', // Gameshow host celebrating
+    'https://media.giphy.com/media/oGO1MPNUVbbk4/giphy.gif', // Small boy thumbs up
+    'https://media.giphy.com/media/uudzUtVcsLAoo/giphy.gif' // Baseballer fist success
+  ];
+
+  // Choose a random reward
+  const chosenReward = Math.floor((Math.random() * (visualRewards.length - 1)) + 1);
+
+  return {
+    attachment: {
+      type: 'image',
+      payload: {
+        url: visualRewards[chosenReward]
+      }
+    }
+  };
+}
 
 function getDifferenceInTimes(baseTime, extendedTime) {
   if (baseTime === 'MORNING' && extendedTime === 'MORNING') {
