3f0426002335ed710b4d6ab32be0cb6bd98fd485
Harry Mumford-Turner <harrymt@me.com>
2017-07-03 10:35:14 +0100

    Increased reward performance
    - Changed button to checkbox for instant interation
    - Added fallback js onclick handler if users don't click straight away
    - If users click straight away, we play the music

--- public/js/reward.js
+++ public/js/reward.js
@@ -2,22 +2,32 @@
 'use strict';
 // TODO dont know if the jslint node: false is working
 
+/**
+ * JS could show the reward with a basic on click handler,
+ * but if users have slow network then their initial press on
+ * the button won't do anything.
+ * This is fixed by adding an onclick handler if users havent already
+ * clicked on the button, otherwise if they have, then we play the
+ * reward.
+ */
 window.onload = function () {
 
   // Get objects
-  const btn = document.getElementsByClassName('btn')[0];
-  const reward = document.getElementsByClassName('container')[0];
+  const btn = document.getElementById('reward-btn');
+  if (btn.checked) {
+    playSound();
+  } else {
+    // Show content when users click the button
+    btn.addEventListener('click', () => {
+      playSound();
+    });
+  }
+};
 
+function playSound() {
   // Sound modality
   const sound = document.getElementById('reward-audio');
-
-  // Show content when users click the button
-  btn.addEventListener('click', function () {
-    btn.style.display = 'none';
-    reward.style.display = 'block';
-
-    if (sound !== null) {
-      sound.play();
-    }
-  });
-};
+  if (sound !== null) {
+    sound.play();
+  }
+}
