77679d55996ff57823172ee6c8fb96f9a3808318
Harry Mumford-Turner <harrymt@me.com>
2017-07-22 09:19:26 +0100

    Fixed js equality operator

--- time.js
+++ time.js
@@ -27,7 +27,7 @@ const properties = () => {
 const value = key => {
   const keys = Object.keys(reminderTimes);
   for (let i = 0; i < keys.length; i++) {
-    if (reminderTimes[keys[i]] === key) {
+    if (reminderTimes[keys[i]] == key) {
       return keys[i];
     }
   }
